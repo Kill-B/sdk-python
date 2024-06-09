267d75e8bf6c9e7cd6980b3260e2563b7f106320
Rafael Andrade <rafaelandrade@Rafaels-MacBook-Pro.local>
2024-06-08 22:48:54 -0300

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="killb-sdk-python",
-    version="1.0.0",
+    version="0.1.0",
     packages=find_packages(),
     requires=['requests'],
     author="Kill-B",
