653314a4b9df0b9ce221b39e6d94c6ea34e3936a
Rafael Andrade <rafaelandrade@Rafaels-MacBook-Pro.local>
2024-06-10 11:52:09 -0300

    Implementation of documentation and release drafter

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     requires=['requests'],
     author="Kill-B",
     description="SDK for KillB API V2",
-    long_description=open('README.md').read(),
+    long_description=open('./README.md').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/Kill-B/sdk-python",
     classifiers=['Programming Language :: Python :: 3',
